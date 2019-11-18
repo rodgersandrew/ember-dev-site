062a87ca15e86feea7f4af3bca2b1758b20ce967
Andrew Rodgers <rodgersandrew@outlook.com>
2019-11-18 16:48:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// var upworkLink = document.querySelector('#upworkLink');
+// var upworkIcon = document.querySelector('#upwork');
+// console.log(upworkIcon);
+
+
+// upworkLink.addEventListener('mouseenter', () => {
+// //   upworkIcon.style.color = '#6FDA44';
+//   upworkIcon.classList.toggle('upwork-hovered')
+//   console.log('entered');
+// });
