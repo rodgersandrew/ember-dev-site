62d72eadbbff3869d4f56d321e3f2ac577e64e5d
Andrew Rodgers <rodgersandrew@outlook.com>
2019-11-21 19:50:33 -0500

    project cards init, animate on scroll in progress

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
-// var upworkLink = document.querySelector('#upworkLink');
-// var upworkIcon = document.querySelector('#upwork');
-// console.log(upworkIcon);
+// Viewport visibility control
+var isInViewport = function(elem) {
+  var bounds = elem.getBoundingClientRect();
+  var top = bounds.top;
+  console.log(top);
+  return (
+    top < (window.innerHeight / 2 || document.documentElement.clientHeight / 2)
+  );
+};
 
+// Projects Handler
 
-// upworkLink.addEventListener('mouseenter', () => {
-// //   upworkIcon.style.color = '#6FDA44';
-//   upworkIcon.classList.toggle('upwork-hovered')
-//   console.log('entered');
-// });
+// Projects -> Header -> Animation Handling
+const projects = document.querySelector('#projects');
+projects.setAttribute('style', 'opacity: 0;');
+const projectsHeader = document.querySelector('#projectsHeader');
+const projectsRule = document.querySelector('#projectsRule');
+
+window.onscroll = () => {
+  if (isInViewport(projects)) {
+    projects.classList.add('animated', 'fadeIn', 'slower');
+  }
+};
+
+// projectsHeader.classList.add('animated', 'fadeIn', 'slower');
+// projectsRule.classList.add('animated', 'fadeIn', 'slower');
+
+// Projects -> Cards -> Animation Handling
+const projectCards = document.querySelectorAll('.projectCard');
+projectCards.forEach(cardContainer => {
+  const cardImg = cardContainer.getElementsByTagName('img')[0];
+  const projectHeader = cardContainer.querySelector('.projectHeader');
+  const projectInfoBtn = cardContainer.querySelector('.projectInfoBtn');
+
+  cardContainer.addEventListener('mouseover', () => {
+    cardImg.classList.add('animated', 'fadeOut');
+    projectHeader.classList.remove('animated', 'fadeOut');
+    projectHeader.classList.add('animated', 'fadeIn');
+    projectInfoBtn.classList.remove('animated', 'fadeOut');
+    projectInfoBtn.classList.add('animated', 'fadeIn');
+  });
+  cardContainer.addEventListener('mouseout', () => {
+    cardImg.classList.remove('animated', 'fadeOut');
+    cardImg.classList.add('animated', 'fadeIn');
+    projectHeader.classList.remove('animated', 'fadeIn');
+    projectHeader.classList.add('animated', 'fadeOut');
+    projectInfoBtn.classList.remove('animated', 'fadeIn');
+    projectInfoBtn.classList.add('animated', 'fadeOut');
+  });
+});
